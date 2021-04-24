2aea5bea9047939e232b894ea472b462125ad905
Artemy <slacky@ya.ru>
2021-04-24 13:28:49 +0300

    shell in client

--- client.c
+++ client.c
@@ -9,15 +9,13 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
-    int sockfd = 0, n = 0;
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr;
+#define BUF_LEN 1024
 
-    if(argc < 2) {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    }
+
+int talk(char *message, char *ip, int port) {
+    int sockfd = 0, n = 0;
+    char recvBuff[BUF_LEN];
+    struct sockaddr_in serv_addr;
 
     memset(recvBuff, '0',sizeof(recvBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -29,9 +27,9 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
+    serv_addr.sin_port = htons(port);
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, ip, &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
@@ -43,18 +41,16 @@ int main(int argc, char *argv[]) {
        return 1;
     }
 
-    n = write(sockfd, argv[2], sizeof(argv[2]));
-    printf("\n Wrote: %d \n", n);
+    n = write(sockfd, message, strlen(message)*sizeof(char));
 
     while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
     {
-	printf("read %d\n", n);
-        recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
-            printf("\n Error : Fputs error\n");
-        }
-	printf("\n");
+            recvBuff[n] = 0;
+            if(fputs(recvBuff, stdout) == EOF)
+            {
+                printf("\n Error : Fputs error\n");
+            }
+    	printf("\n");
     }
 
     if(n < 0)
@@ -64,3 +60,56 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+
+int main(int argc, char *argv[]) {
+
+    char *program_name = argv[0];
+    if(argc < 3) {
+        printf("Usage: %s <ip of server> <port>\n", program_name);
+        return 1;
+    }
+
+    char *ip = argv[1];
+    int port;
+    if (!(sscanf(argv[2], "%d", &port))) {
+        printf("invalid port\n");
+    }
+
+    char *command = (char*) malloc(sizeof(char) * BUF_LEN);
+    int ret;
+    printf("Intro\n");
+
+    // if (!(fopen(title, "r")))
+    //     return 1;
+
+    printf("enter \'help\' to see all available commands\n");
+    printf("> ");
+    ret = scanf("%s", command);
+
+
+    while (strcmp(command, "exit") && (ret != EOF)) {
+        if (strstr(command, "touch")) {
+            printf("called touch\n");
+            if (!(ret = talk(command, ip, port))) {
+                printf("ok!\n");
+            }
+        }
+        else if (strstr(command, "rm")) {
+            printf("called rm\n");
+
+        }
+        else if(strstr(command, "help")) {
+            printf("Commands:\n"
+            "touch \n"
+            "rm \n");
+        }
+
+        printf("> ");
+        ret = scanf("%s", command);
+    }
+
+}
+
+
+
